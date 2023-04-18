9737423124b7077cc8f0f3de54c51b4ab24c0d02
Cheryl L <cheryl.loeffler@gmail.com>
2023-04-18 12:24:29 -0600

    added questions array

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return`[![License: ](${license.badge})]`
+  //get links to licenses and badges
 }
 
 // TODO: Create a function that returns the license link
--- index.js
+++ index.js
@@ -3,14 +3,69 @@ const generateMarkdown = require('./generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+    {
+        type: 'input',
+        message: 'Describe your project: ',
+        name: 'description',
+      },
+    {
+        type: 'input',
+        message: 'Add installation instructions',
+        name: 'installation',
+      },
+    {
+        type: 'input',
+        message: 'Add usage instructions/examples',
+        name: 'usage',
+      },
+    {
+        type: 'list',
+        message: 'What kind of license would you like?',
+        choices: ['MIT (very permissive)', 'GNU (less permissive)'],
+        //https://choosealicense.com/licenses/mit/
+        //https://choosealicense.com/licenses/gpl-3.0/
+        name: 'license',
+      },
+    {
+      type: 'input',
+      message: 'Any contributing guidelines you want to include?',
+      name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Add examples of how to run any tests ',
+        name: 'tests',
+      },
+    {
+        type: 'input',
+        message: 'What is your GitHub username',
+        name: 'username',
+      },
+    {
+        type: 'input',
+        message: 'What is your email address',
+        name: 'email',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer
+  .prompt(...questions)
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
 }
 
 // Function call to initialize app
